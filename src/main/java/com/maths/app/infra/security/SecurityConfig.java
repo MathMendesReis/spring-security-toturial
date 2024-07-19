bef66b1d09ecb0665122f2596b6e5016ac9403e5
Matheus Mendes <matheusmendesreis@outlook.com>
2024-07-19 18:38:06 -0300

    feat: setup security config

--- src/main/java/com/maths/app/infra/security/SecurityConfig.java
+++ src/main/java/com/maths/app/infra/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.maths.app.infra.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,23 +11,27 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
     @Autowired
+    SecurityFilter securityFilter;
+
+  private static final String[] PERMIT_ALL_LIST = {
+    "/auth/**"
+};
 
     @Bean
-    /*
-     * Corrente de filtros
-     */
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .anyRequest().permitAll())
+                        .requestMatchers(PERMIT_ALL_LIST).permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
